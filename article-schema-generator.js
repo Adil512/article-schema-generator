dee67278befbabb93fa12572c32adfd28f6e0482
Muhammad Adil <alijee2929@gmail.com>
2024-08-27 20:57:36 +0400

    Create article-schema-generator.js
    Here is how you can create an Article Schema for your website that can help to get a better rank in Google and other search engines. Balti Bloggers is a free Schema Generator and validator online for SEOs and Developers.

---
+++ article-schema-generator.js
@@ -0,0 +1,29 @@
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "NewsArticle",
+  "mainEntityOfPage": {
+    "@type": "WebPage",
+    "@id": "https://example.com/"
+  },
+  "headline": "Article Schema Generator Tool and Validator",
+  "image": [
+    "https://example.com/logo.png"
+  ],
+  "author": {
+    "@type": "Person",
+    "name": "AuthorName",
+    "url": "https://example.com/author"
+  },
+  "datePublished": "2024-04-18",
+  "dateModified": "2024-07-18",
+  "publisher": {
+    "@type": "Organization",
+    "name": "AuthorName",
+    "logo": {
+      "@type": "ImageObject",
+      "url": "https://example.com/author.logo"
+    }
+  }
+}
+  </script>
